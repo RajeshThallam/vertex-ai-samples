6cc19b474335fb8485bc9c5a48a45b8e1c260dc2
Justin Marciszewski <justinjm@users.noreply.github.com>
2022-04-13 12:21:35 -0400

    successful model training and upload to GCS

--- community-content/r_train_and_deploy/scripts/01_setup.R
+++ community-content/r_train_and_deploy/scripts/01_setup.R
@@ -1,18 +1,16 @@
 # setup.R -----------------------------------------------------------------
 
-## Setup local environment
-
-
 ## Setup GCP project
 
 # https://ssh.cloud.google.com/cloudshell/editor
 
-export PROJECT=$DEVSHELL_PROJECT_ID
+# export PROJECT=$DEVSHELL_PROJECT_ID
+# 
+# gcloud services enable --project $PROJECT notebooks.googleapis.com
+# gcloud services enable --project $PROJECT aiplatform.googleapis.com
+# gcloud services enable --project $PROJECT artifactregistry.googleapis.com
 
-gcloud services enable --project $PROJECT notebooks.googleapis.com
-gcloud services enable --project $PROJECT aiplatform.googleapis.com
-gcloud services enable --project $PROJECT artifactregistry.googleapis.com
-
+### Not needed ###
 # gcloud services enable --project $PROJECT monitoring.googleapis.com
 # gcloud services enable --project $PROJECT logging.googleapis.com
 # gcloud services enable --project $PROJECT compute.googleapis.com
@@ -22,3 +20,29 @@ gcloud services enable --project $PROJECT artifactregistry.googleapis.com
 
 ### Create GCS bucket 
 
+## set default project and bucket via environment in .Renviron 
+project_id <- Sys.getenv("GCP_PROJECT_ID")
+bucket <- Sys.getenv("GCS_BUCKET")
+email <- Sys.getenv("GARGLE_AUTH_EMAIL")
+
+# load packages 
+library(googleCloudStorageR)
+library(gargle)
+
+## Fetch token. See: https://developers.google.com/identity/protocols/oauth2/scopes
+scope <- c("https://www.googleapis.com/auth/cloud-platform")
+token <- token_fetch(scopes = scope,
+                     email = email)
+
+## Pass your token to gcs_auth
+gcs_auth(token = token)
+
+## create bucket 
+gcs_create_bucket(
+  name = bucket,
+  projectId = project_id,
+  location = "us-central1",
+  storageClass = "REGIONAL")
+
+## check again to confirm bucket creation successful 
+gcs_get_bucket(bucket = bucket)
--- community-content/r_train_and_deploy/scripts/03_train.R
+++ community-content/r_train_and_deploy/scripts/03_train.R
@@ -1,11 +1,36 @@
 # 02_train.R --------------------------------------------------------------
 
+## load packages 
 library(here)
 library(randomForest)
 
-# train model
+library(googleCloudStorageR)
+library(gargle)
+
+## set default project and bucket via environment in .Renviron 
+project_id <- Sys.getenv("GCP_PROJECT_ID")
+bucket <- Sys.getenv("GCS_BUCKET")
+email <- Sys.getenv("GARGLE_AUTH_EMAIL")
+
+
+## train model
 model <- randomForest(Species ~ ., data = iris)
 
-# save model
+## save model
 save(model, file = here("container","model.RData"))
 
+## upload model file to GCS for deployment/serving
+## https://cran.r-project.org/web/packages/googleCloudStorageR/vignettes/googleCloudStorageR.html
+
+### Fetch token. See: https://developers.google.com/identity/protocols/oauth2/scopes
+scope <- c("https://www.googleapis.com/auth/cloud-platform")
+token <- token_fetch(scopes = scope,
+                     email = email)
+
+### Pass your token to gcs_auth
+gcs_auth(token = token)
+
+### execute upload 
+gcs_upload(file = here("container","model.RData"),
+           name = "model.RData",
+           bucket = bucket)
